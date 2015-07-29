b87c9c9db2aefd9cf3f0ec5ff2fa9fa68c5c6109
Matt Revelle <mrevelle@gmail.com>
2015-07-29 17:49:25 -0400

    Updated deps to point to release versions. Updated description.

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
 (defproject senc "0.1.1"
-  :description "Seeded estimation of network communities. Estmates community topics and node memberships."
+  :description "Seeded estimation of network communities. Implements the method proposed in \"Finding Community Topics and Membership in Graphs\"."
   :url "https://github.com/gmu-dmml-lab/senc"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [prismatic/schema "0.4.3"]
-                 [notion "0.1.0-SNAPSHOT"]
-                 [munge "0.1.0-SNAPSHOT"]
-                 [net.mikera/core.matrix "0.33.3-SNAPSHOT"]
-                 [net.mikera/vectorz-clj "0.29.1-SNAPSHOT"]]
+                 [notion "0.1.1"]
+                 [munge "0.1.1"]
+                 ;;[net.mikera/core.matrix "0.33.3-SNAPSHOT"]
+                 ;;[net.mikera/vectorz-clj "0.29.1-SNAPSHOT"]
+                 [net.mikera/core.matrix "0.36.1"]
+                 [net.mikera/vectorz-clj "0.30.1"]]
   :main senc.core
   :profiles {:common {:jvm-opts ["-Xmx14g" "-server" "-XX:+UseConcMarkSweepGC" "-XX:+TieredCompilation"]}
              :prod [:common]}
